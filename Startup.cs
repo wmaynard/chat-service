75ee662ed0424a57fc0c521a8e8fbacc1eb2ca6c
Will Maynard <will@willmaynard.com>
2022-08-11 12:51:09 -0700

    Bump to common 1.1.53 for logs

--- Startup.cs
+++ Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.Extensions.DependencyInjection;
 using RCL.Logging;
+using Rumble.Platform.Common.Enums;
 using Rumble.Platform.Common.Utilities;
 using Rumble.Platform.Common.Web;
 
@@ -9,5 +10,6 @@ public class Startup : PlatformStartup
 {
 	protected override PlatformOptions Configure(PlatformOptions options) => options
 		.SetProjectOwner(Owner.Will)
-		.SetPerformanceThresholds(warnMS: 500, errorMS: 2_000, criticalMS: 30_000);
+		.SetPerformanceThresholds(warnMS: 500, errorMS: 2_000, criticalMS: 30_000)
+		.DisableFeatures(CommonFeature.LogglyThrottling);
 }
