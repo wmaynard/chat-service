b3624d66c9296b2bca6bfbf41b58df6350ccc76c
Sean Chapel <sean@seoushigames.com>
2024-06-06 13:10:39 -0600

    updated platform common

--- Program.cs
+++ Program.cs
@@ -8,18 +8,18 @@ namespace Rumble.Platform.ChatService;
 
 public class Program
 {
-	public static void Main(string[] args)
-	{
-		if (args.Contains("-version"))
-		{
-			AssemblyName assembly = Assembly.GetExecutingAssembly().GetName();
-			Console.WriteLine($"{assembly.Name}:{assembly.Version}");
-			return;
-		}
-		CreateHostBuilder(args).Build().Run();
-	}
+  public static void Main(string[] args)
+  {
+    if (args.Contains("-version"))
+    {
+      AssemblyName assembly = Assembly.GetExecutingAssembly().GetName();
+      Console.WriteLine($"{assembly.Name}:{assembly.Version}");
+      return;
+    }
+    CreateHostBuilder(args).Build().Run();
+  }
 
-	public static IHostBuilder CreateHostBuilder(string[] args) =>
-		Host.CreateDefaultBuilder(args)
-			.ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+  public static IHostBuilder CreateHostBuilder(string[] args) =>
+    Host.CreateDefaultBuilder(args)
+      .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
 }
